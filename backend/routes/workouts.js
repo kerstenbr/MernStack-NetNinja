0d60784269cc807b1f374b268edd1be6564962e3
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-05-11 14:29:18 -0300

    aula

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -13,18 +14,25 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', (req, res) => {
-    res.json({ msg: "POST a workout" })
+router.post('/', async (req, res) => {
+    const { title, load, reps } = req.body
+
+    try {
+        const workout = await Workout.create({ title, load, reps })
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 })
 
 // DELETE a workout
 router.delete('/:id', (req, res) => {
-    res.json({msg: "DELETE a workout"})
+    res.json({ msg: "DELETE a workout" })
 })
 
 // UPDATE a workout
 router.patch('/:id', (req, res) => {
-    res.json({msg: "UPDATE a workout"})
+    res.json({ msg: "UPDATE a workout" })
 })
 
 module.exports = router
