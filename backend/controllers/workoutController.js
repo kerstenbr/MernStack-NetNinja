39e459f3df9032a49606206396c752f2d476a862
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-05-11 14:58:07 -0300

    aula

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -17,14 +17,14 @@ const getWorkout = async (req, res) => {
     try {
         const { id } = req.params
 
-        if(!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(404).json({error: 'Invalid mongo id'})
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ error: 'Invalid mongo id' })
         }
 
         const workout = await Workout.findById(id)
 
-        if(!workout) {
-            return res.status(404).json({error: 'No such workout'})
+        if (!workout) {
+            return res.status(404).json({ error: 'No such workout' })
         }
 
         res.status(200).json(workout)
@@ -46,11 +46,53 @@ const createWorkout = async (req, res) => {
 }
 
 // delete a workout
+const deleteWorkout = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ error: 'Invalid mongo id' })
+        }
+
+        const workout = await Workout.findOneAndDelete({ _id: id })
+
+        if (!workout) {
+            return res.status(400).json({ error: 'No such workout' })
+        }
+
+        res.status(200).json({ workout })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
 
 // update a workout
+const updateWorkout = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ error: 'Invalid mongo id' })
+        }
+
+        const workout = await Workout.findOneAndUpdate({ _id: id }, {
+            ...req.body
+        })
+
+        if (!workout) {
+            return res.status(400).json({ error: 'No such workout' })
+        }
+
+        res.status(200).json({ workout })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
 
 module.exports = {
     createWorkout,
     getWorkouts,
-    getWorkout
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
 }
