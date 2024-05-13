bdb1437ddde8d98e2fe4d31526da7958fa3a477d
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-05-13 15:19:52 -0300

    aula

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,25 @@
 const User = require("../models/userModel")
+const jwt = require("jsonwebtoken")
 // const mongoose = require("mongoose")
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET_JWT, { expiresIn: '3d' })
+}
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({ msg: "login user" })
+    try {
+        const {email, password} = req.body
+
+        const user = await User.login(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(201).json({ email, token })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 //signup user
@@ -13,7 +29,10 @@ const signupUser = async (req, res) => {
 
         const user = await User.signup(email, password)
 
-        res.status(201).json({ email, user })
+        // create a token
+        const token = createToken(user._id)
+        
+        res.status(201).json({ email, token })
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
