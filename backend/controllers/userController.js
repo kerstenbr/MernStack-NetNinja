9c022f1a7a8149639dc13d3e47dab7536dd888a4
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-05-13 14:23:15 -0300

    Adicionar hash de senha e validação

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,15 @@ const loginUser = async (req, res) => {
 
 //signup user
 const signupUser = async (req, res) => {
-    res.json({ msg: "signup user" })
+    try {
+        const { email, password } = req.body
+
+        const user = await User.signup(email, password)
+
+        res.status(201).json({ email, user })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 module.exports = { loginUser, signupUser }
