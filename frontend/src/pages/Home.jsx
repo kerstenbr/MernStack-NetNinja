0b124f7eee0cb97f7d317968e94e6c4b1bf89c1e
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-05-15 11:05:09 -0300

    aula

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,31 @@
 import { useEffect } from "react"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from '../hooks/useAuthContext'
 
 import WorkoutDetails from "../components/WorkoutDetails"
 import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
     const { workouts, dispatch } = useWorkoutsContext()
+    const { user } = useAuthContext()
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('http://localhost:4000/api/workouts')
+            const response = await fetch('http://localhost:4000/api/workouts', {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            })
             const json = await response.json()
 
             if (response.ok) {
-                dispatch({type: "SET_WORKOUTS", payload: json})
+                dispatch({ type: "SET_WORKOUTS", payload: json })
             }
         }
+        if (user) {
+            fetchWorkouts()
+        }
 
-        fetchWorkouts()
-    }, [dispatch])
+    }, [dispatch, user])
 
     return (
         <div className="home">
